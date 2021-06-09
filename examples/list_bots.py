6a713ab71bc4368cd52cfc5c72aa905fb70408da
Adam Kelly <adam@cthulahoops.org>
2021-06-09 12:02:41 +0100

    Add some examples.

---
+++ examples/list_bots.py
@@ -0,0 +1,8 @@
+import asyncio
+from rctogether import bots, RestApiSession
+
+async def main():
+    async with RestApiSession() as session:
+        print(await bots.get(session))
+
+asyncio.run(main())
---
+++ examples/print_stream.py
@@ -0,0 +1,8 @@
+import asyncio
+from rctogether import WebsocketSubscription
+
+async def main():
+    async for message in WebsocketSubscription():
+        print(message)
+
+asyncio.run(main())
