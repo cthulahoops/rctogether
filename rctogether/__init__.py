b14ef6a78291ac79c38515b91ed804d0a47e6859
Adam Kelly <adam@cthulahoops.org>
2021-06-16 15:15:00 +0100

    Bump version number -> 0.3.0
    Small API changes:
      - Enviroment re-read every time a new connection is created.
      - Stricter argument parsing for bot creation.
      - Bug fix changes the behaviour of wall creation.

--- rctogether/__init__.py
+++ rctogether/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 
 from .websocket_subscription import WebsocketSubscription
 from .api import RestApiSession
--- tests/test_rctogether.py
+++ tests/test_rctogether.py
@@ -5,7 +5,7 @@ from .fixtures import server, session
 
 
 def test_version():
-    assert __version__ == "0.2.2"
+    assert __version__ == "0.3.0"
 
 
 @pytest.mark.asyncio
